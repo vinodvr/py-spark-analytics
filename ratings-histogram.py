7082dd35dddc6eb2ae7937764b0032fa6e98c387
Vinod V <vinod@instaguideapp.com>
2017-07-16 23:21:02 +0530

    working ratings histogra

--- ratings-histogram.py
+++ ratings-histogram.py
@@ -4,10 +4,11 @@ import collections
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:///Users/vinodvr/spark-course/ml-100k/u.data")
+lines = sc.textFile("file:///Users/vinodvr/spark-python/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
+print('Rating Count')
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
