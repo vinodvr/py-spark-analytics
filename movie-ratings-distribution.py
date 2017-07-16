2785447fe3257961d33254c80ae1d385968f0ea4
Vinod V <vinod@instaguideapp.com>
2017-07-17 00:27:05 +0530

    movie ratings distribution

--- movie-ratings-distribution.py
+++ movie-ratings-distribution.py
@@ -3,13 +3,15 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("MovieRatingsDistribution")
 sc = SparkContext(conf = conf)
 
-movieId = 127 # movieID of Godfather is 127
-
+movieId = '127'   # movieID of Godfather is 127
+movieIdColIdx = 1 # 2nd column has movieId in data file
+ratingColIdx = 2  # 3rd column has the actual rating given by a user
+ 
 linesRDD = sc.textFile("file:///Users/vinodvr/spark-python/ml-100k/u.data")
-movieRatingsRDD = linesRDD.filter(lambda x: int(x.split()[1]) == movieId)
+movieRatingsRDD = linesRDD.filter(lambda x: x.split()[movieIdColIdx] == movieId)
 
 ratingsHistogram = movieRatingsRDD \
-                .map(lambda x: (int(x.split()[2]), 1)) \
+                .map(lambda x: (x.split()[ratingColIdx], 1)) \
                 .reduceByKey(lambda a, b: a + b) \
                 .collect()
 
