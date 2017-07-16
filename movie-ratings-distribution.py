8e4d11d1ca7776567d1e40d58823b5ccbd1d1134
Vinod V <vinod@instaguideapp.com>
2017-07-17 02:22:39 +0530

    top rated movies using spark sql

--- movie-ratings-distribution.py
+++ movie-ratings-distribution.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 
+# create SparkContext using standalone mode
 conf = SparkConf().setMaster("local").setAppName("MovieRatingsDistribution")
 sc = SparkContext(conf = conf)
 
@@ -7,9 +8,11 @@ movieId = '127'   # movieID of Godfather is 127
 movieIdColIdx = 1 # 2nd column has movieId in data file
 ratingColIdx = 2  # 3rd column has the actual rating given by a user
  
+ # create the RDD and filter it to only include desired movie ratings
 linesRDD = sc.textFile("file:///Users/vinodvr/spark-python/ml-100k/u.data")
 movieRatingsRDD = linesRDD.filter(lambda x: x.split()[movieIdColIdx] == movieId)
 
+# run a mapper and reducer function to calculate rating distribution
 ratingsHistogram = movieRatingsRDD \
                 .map(lambda x: (x.split()[ratingColIdx], 1)) \
                 .reduceByKey(lambda a, b: a + b) \
--- ratings-histogram.py
+++ ratings-histogram.py
@@ -1,13 +1,18 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
+# create SparkContext using standalone mode
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
+# create a RDD from text file and transform it to only contains ratings column
 linesRDD = sc.textFile("file:///Users/vinodvr/spark-python/ml-100k/u.data")
 ratingsRDD = linesRDD.map(lambda x: x.split()[2])
+
+# use RDD action countByValue to count how many times each value occurs..
 result = ratingsRDD.countByValue()
 
+# print out the results
 sortedResults = collections.OrderedDict(sorted(result.items()))
 print('Rating Count')
 for key, value in sortedResults.items():
